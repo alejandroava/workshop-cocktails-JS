0db1876ea279943f019b0aab2468b98455cab532
alejandroava <alejandroalzate.ava@gmail.com>
2024-07-22 12:15:22 -0500

    add progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,24 @@
 // Aquí puedes agregar la lógica para procesar el registro
+
+document.getElementById("register-form").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    const password = document.getElementById("password").value
+    const email = document.getElementById("email").value
+    const nombre = document.getElementById("nombre").value
+
+    const enviarDatos = await fetch("http://localhost:3000/users", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            password,
+            nombre,
+            email
+        })
+    })
+
+
+
+})
 // Aquí puedes agregar la lógica para procesar el inicio de sesión
--- scripts/service.js
+++ scripts/service.js
@@ -1,1 +1,10 @@
 // Codigo de las peticiones
+async function FecthApi(url,options) {
+    try {
+        const resp = await fetch(url,options)
+        const data = resp.json()
+        console.log(data)
+    } catch {
+        console.log("Error con la data")
+    }
+}
